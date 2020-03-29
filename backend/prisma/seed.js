26dc2ce8446d7970ab15c66705f126b003a67b17
Luke Hamilton <lukehamiltonmail@gmail.com>
2020-03-28 23:32:10 -0400

    WIP

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -108,139 +108,139 @@ async function main() {
   }
 
   // Create posts
-  for (let i = 0; i < posts.length; i += 1) {
-    console.log('posts', i)
-    const submitter = usernames[Math.floor(Math.random() * usernames.length)]
-    const { minCreators, maxCreators } = config.creators
-    const selectedCreators = usernames.slice(
-      0,
-      Math.floor(Math.random() * (maxCreators - minCreators + 1)) + minCreators,
-    )
-    const connectedCreators = selectedCreators.map(username => ({ username }))
+  // for (let i = 0; i < posts.length; i += 1) {
+  //   console.log('posts', i)
+  //   const submitter = usernames[Math.floor(Math.random() * usernames.length)]
+  //   const { minCreators, maxCreators } = config.creators
+  //   const selectedCreators = usernames.slice(
+  //     0,
+  //     Math.floor(Math.random() * (maxCreators - minCreators + 1)) + minCreators,
+  //   )
+  //   const connectedCreators = selectedCreators.map(username => ({ username }))
 
-    await prisma.post.create({
-      data: {
-        ...posts[i],
-        link: 'https://loomx.io/',
-        galleryThumbs: { set: galleryThumbs },
-        submitter: { connect: { username: submitter } },
-        creators: { connect: connectedCreators },
-        description:
-          'Put the outside world on hold – this is all about you and your music. No noise, no wires, no distractions. Just exceptional sound, industry-leading noise cancellation, and hour upon hour of pure listening freedom.',
-      },
-    })
-  }
+  //   await prisma.post.create({
+  //     data: {
+  //       ...posts[i],
+  //       link: 'https://loomx.io/',
+  //       galleryThumbs: { set: galleryThumbs },
+  //       submitter: { connect: { username: submitter } },
+  //       creators: { connect: connectedCreators },
+  //       description:
+  //         'Put the outside world on hold – this is all about you and your music. No noise, no wires, no distractions. Just exceptional sound, industry-leading noise cancellation, and hour upon hour of pure listening freedom.',
+  //     },
+  //   })
+  // }
 
   // Create sections
-  for (let i = 0; i < config.sections.count; i += 1) {
-    console.log('section', i)
-    const date = moment()
-      .subtract(i, 'd')
-      .format('YYYY-MM-DD')
+  // for (let i = 0; i < config.sections.count; i += 1) {
+  //   console.log('section', i)
+  //   const date = moment()
+  //     .subtract(i, 'd')
+  //     .format('YYYY-MM-DD')
 
-    postSlugs.sort(() => 0.5 - Math.random())
-    const { minPosts, maxPosts } = config.sections
-    const selectedPostSlugs = postSlugs.slice(
-      0,
-      Math.floor(Math.random() * (maxPosts - minPosts + 1)) + minPosts,
-    )
-    const connectedPostSlugs = selectedPostSlugs.map(slug => ({ slug }))
-    await prisma.section.create({
-      data: {
-        date,
-        posts: { connect: connectedPostSlugs },
-      },
-    })
-  }
+  //   postSlugs.sort(() => 0.5 - Math.random())
+  //   const { minPosts, maxPosts } = config.sections
+  //   const selectedPostSlugs = postSlugs.slice(
+  //     0,
+  //     Math.floor(Math.random() * (maxPosts - minPosts + 1)) + minPosts,
+  //   )
+  //   const connectedPostSlugs = selectedPostSlugs.map(slug => ({ slug }))
+  //   await prisma.section.create({
+  //     data: {
+  //       date,
+  //       posts: { connect: connectedPostSlugs },
+  //     },
+  //   })
+  // }
 
   // Create votes
-  for (let i = 0; i < postSlugs.length; i += 1) {
-    console.log('votes', i)
-    const { minVotes, maxVotes } = config.votes
-    const selectedUsernames = usernames.slice(
-      0,
-      Math.floor(Math.random() * (maxVotes - minVotes + 1)) + minVotes,
-    )
-    for (let j = 0; j < selectedUsernames.length; j += 1) {
-      await prisma.vote.create({
-        data: {
-          user: { connect: { username: selectedUsernames[j] } },
-          post: { connect: { slug: postSlugs[i] } },
-        },
-      })
-    }
-  }
+  // for (let i = 0; i < postSlugs.length; i += 1) {
+  //   console.log('votes', i)
+  //   const { minVotes, maxVotes } = config.votes
+  //   const selectedUsernames = usernames.slice(
+  //     0,
+  //     Math.floor(Math.random() * (maxVotes - minVotes + 1)) + minVotes,
+  //   )
+  //   for (let j = 0; j < selectedUsernames.length; j += 1) {
+  //     await prisma.vote.create({
+  //       data: {
+  //         user: { connect: { username: selectedUsernames[j] } },
+  //         post: { connect: { slug: postSlugs[i] } },
+  //       },
+  //     })
+  //   }
+  // }
 
   // Create comments
-  for (let i = 0; i < postSlugs.length; i += 1) {
-    console.log('comments', i)
-    const { minComments, maxComments, minReplies, maxReplies } = config.comments
+  // for (let i = 0; i < postSlugs.length; i += 1) {
+  //   console.log('comments', i)
+  //   const { minComments, maxComments, minReplies, maxReplies } = config.comments
 
-    for (let j = minComments; j < maxComments; j += 1) {
-      const author = usernames[Math.floor(Math.random() * usernames.length)]
-      const parent = await prisma.comment.create({
-        data: {
-          author: { connect: { username: author } },
-          post: { connect: { slug: postSlugs[i] } },
-          text: COMMENT_PARENT_TEXT,
-        },
-      })
+  //   for (let j = minComments; j < maxComments; j += 1) {
+  //     const author = usernames[Math.floor(Math.random() * usernames.length)]
+  //     const parent = await prisma.comment.create({
+  //       data: {
+  //         author: { connect: { username: author } },
+  //         post: { connect: { slug: postSlugs[i] } },
+  //         text: COMMENT_PARENT_TEXT,
+  //       },
+  //     })
 
-      const selectedUsernames = usernames.slice(
-        0,
-        Math.floor(
-          Math.random() * (config.votes.maxVotes - config.votes.minVotes + 1),
-        ) + config.votes.minVotes,
-      )
-      for (let h = 0; h < selectedUsernames.length; h += 1) {
-        await prisma.commentVote.create({
-          data: {
-            user: { connect: { username: selectedUsernames[0] } },
-            comment: { connect: { id: parent.id } },
-          },
-        })
-      }
+  //     const selectedUsernames = usernames.slice(
+  //       0,
+  //       Math.floor(
+  //         Math.random() * (config.votes.maxVotes - config.votes.minVotes + 1),
+  //       ) + config.votes.minVotes,
+  //     )
+  //     for (let h = 0; h < selectedUsernames.length; h += 1) {
+  //       await prisma.commentVote.create({
+  //         data: {
+  //           user: { connect: { username: selectedUsernames[0] } },
+  //           comment: { connect: { id: parent.id } },
+  //         },
+  //       })
+  //     }
 
-      for (let k = minReplies; k < maxReplies; k += 1) {
-        const replyAuthor =
-          usernames[Math.floor(Math.random() * usernames.length)]
-        const reply = await prisma.comment.create({
-          data: {
-            author: { connect: { username: replyAuthor } },
-            text: COMMENT_REPLY_TEXT,
-            parent: { connect: { id: parent.id } },
-          },
-        })
+  //     for (let k = minReplies; k < maxReplies; k += 1) {
+  //       const replyAuthor =
+  //         usernames[Math.floor(Math.random() * usernames.length)]
+  //       const reply = await prisma.comment.create({
+  //         data: {
+  //           author: { connect: { username: replyAuthor } },
+  //           text: COMMENT_REPLY_TEXT,
+  //           parent: { connect: { id: parent.id } },
+  //         },
+  //       })
 
-        const selectedUsernames = usernames.slice(
-          0,
-          Math.floor(
-            Math.random() * (config.votes.maxVotes - config.votes.minVotes + 1),
-          ) + config.votes.minVotes,
-        )
+  //       const selectedUsernames = usernames.slice(
+  //         0,
+  //         Math.floor(
+  //           Math.random() * (config.votes.maxVotes - config.votes.minVotes + 1),
+  //         ) + config.votes.minVotes,
+  //       )
 
-        for (let l = 0; l < selectedUsernames.length; l += 1) {
-          await prisma.commentVote.create({
-            data: {
-              user: { connect: { username: selectedUsernames[0] } },
-              comment: { connect: { id: reply.id } },
-            },
-          })
-        }
+  //       for (let l = 0; l < selectedUsernames.length; l += 1) {
+  //         await prisma.commentVote.create({
+  //           data: {
+  //             user: { connect: { username: selectedUsernames[0] } },
+  //             comment: { connect: { id: reply.id } },
+  //           },
+  //         })
+  //       }
 
-        // replyIds.push(reply.id)
-      }
-      // const connectedreplyIds = replyIds.map(id => ({ id }))
-      // for (let m = 0; m < connectedreplyIds; m++) {}
+  //       // replyIds.push(reply.id)
+  //     }
+  //     // const connectedreplyIds = replyIds.map(id => ({ id }))
+  //     // for (let m = 0; m < connectedreplyIds; m++) {}
 
-      // await prisma.comment.update({
-      //   where: { id: parent.id },
-      //   data: {
-      //     replies: { connect: connectedreplyIds },
-      //   },
-      // })
-    }
-  }
+  //     // await prisma.comment.update({
+  //     //   where: { id: parent.id },
+  //     //   data: {
+  //     //     replies: { connect: connectedreplyIds },
+  //     //   },
+  //     // })
+  //   }
+  // }
 }
 
 main()
