2acf2bb57f918683925cc84a808d793027f76d8d
Luke Hamilton <lukehamiltonmail@gmail.com>
2020-04-12 22:43:51 -0400

    please work 2

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -45,7 +45,8 @@ const IndexPage = ({ user }) => {
 
   return (
     <Page>
-      <div>{process ? process.env.APP_ENV : "process unavailable"}</div>
+      <h1>Another Test</h1>
+      <h1>{process ? process.env.APP_ENV : "process unavailable"}</h1>
       <StyledContainer>
         <Sidebar>
           {currentUser && (
