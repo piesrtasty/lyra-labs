4e4502f699b8c1f26932565090252408a2a0c0ed
Luke Hamilton <lukehamiltonmail@gmail.com>
2020-04-28 22:55:15 -0400

    WIP

--- native/App.js
+++ native/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -16,99 +8,39 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Test from './components/test';
 
-const App: () => React$Node = () => {
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider} from '@apollo/react-hooks';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+const BACKEND_URL = 'http://localhost:4000/graphql';
+
+// create an apollo link instance, a network interface for apollo client
+const link = new HttpLink({
+  uri: BACKEND_URL,
+});
+// create an inmemory cache instance for caching graphql data
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <Test />
+        <Text>Cool Guy</Text>
       </SafeAreaView>
-    </>
+    </ApolloProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ native/components/test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {View, Text} from 'react-native';
+import {useQuery} from '@apollo/react-hooks';
+import {withApollo} from 'react-apollo';
+
+const POSTS_QUERY = gql`
+  query post {
+    posts {
+      id
+    }
+  }
+`;
+
+const Test = () => {
+  const {data, error, loading} = useQuery(POSTS_QUERY);
+
+  return (
+    <View>
+      <Text>The test component</Text>
+      {loading && <Text>Loading....</Text>}
+      {!loading && data && (
+        <View>
+          <Text>{JSON.stringify(data)}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default withApollo(Test);
