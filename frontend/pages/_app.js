9c50af501ba4de575cae14e4ab703dcdbd9984ee
Luke Hamilton <lukehamiltonmail@gmail.com>
2021-04-05 16:54:31 -0400

    more cleanup

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -6,16 +6,8 @@ import { withMagicAuth } from "@enhancers/magic-auth";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { withMobile } from "../shared/enhancers/mobile-enhancer";
-import flowConfig from "@config/flow";
-
-const { ACCESS_NODE_API, CHALLENGE_HANDSHAKE } = flowConfig;
 
 toast.configure();
-
-fcl
-  .config()
-  .put("accessNode.api", ACCESS_NODE_API)
-  .put("challenge.handshake", CHALLENGE_HANDSHAKE);
 
 import { Magic } from "magic-sdk";
 
